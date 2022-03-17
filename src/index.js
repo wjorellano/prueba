4f6cd605b502fe0c5438fd410b6d6deec3f88fad
willman orellano heredia <wilmanjunior2001@gmail.com>
2022-03-17 09:36:05 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const container = document.getElementById('root');
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  container
 );
 
 // If you want to start measuring performance in your app, pass a function
