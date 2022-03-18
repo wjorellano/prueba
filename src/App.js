7efd48b0bbf67b902673888b15ad05366f99577f
willman orellano heredia <wilmanjunior2001@gmail.com>
2022-03-18 07:11:40 -0500

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import Home from './pages/Home.jsx';
-import NotFound from './pages/NotFound.jsx';
+// import NotFound from './pages/NotFound.jsx';
 
 function App() {
   return (
     <div className="App container-fluid">
       <Router>
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home/>} />
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </Router>
     </div>
